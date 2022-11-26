608caa167b2e676c48eeb3f298d11f8d1c6d007e
ncinsli <56553344+ncinsli@users.noreply.github.com>
2022-11-27 00:04:42 +0500

    Improved LCD displaying system and fixed some callbacks

--- daemon/daemon.py
+++ daemon/daemon.py
@@ -12,28 +12,34 @@ class Daemon(threading.Thread):
     today_timetable: list
     muted_rings: list
     last_called_timing: str = '00:00'
+    next_called_timing: str = '00:00'
+    screen = None
     status: bool = True
 
     def __init__(self, timetable, muted):
         super().__init__()
         self.timetable = storage.TimetableStorage()
         self.update(timetable, muted)
+        self.screen = LCD()
+        self.screen.message('System started')
 
     def update(self, new_timetable, new_muted):
         self.today_timetable, self.muted_rings = new_timetable, new_muted # Обращаться к sqlite из другого потока нельзя
         self.today_timetable = list(map(lambda e: e.zfill(5), self.today_timetable))
         print(colored('[DAEMON] ', 'blue') + "Updated timetable:", self.today_timetable)
-        screen = LCD()
-        screen.message('Timetable updated')
+        self.screen.message('Timetable updated')
+        time.sleep(5)
+        self.screen.clear()
         print(colored('[DAEMON] ', 'blue') + "Updated muted list:", *self.muted_rings)
 
     def run(self):
-
         while self.status:
             time.sleep(3)
             timing = str(datetime.now().time())[:5]
 
             if (timing in self.today_timetable and timing != self.last_called_timing):
+                self.screen.message.clear()
+
                 ring_order = self.today_timetable.index(str(datetime.now().time())[:5])
 
                 if self.muted_rings[ring_order] == 0:
@@ -41,10 +47,13 @@ class Daemon(threading.Thread):
                     self.last_called_timing = timing
                     time.sleep(self.ring_duration)
                     ring_callbacks.stop_ring()
+                    self.next_called_timing = str(self.today_timetable[self.today_timetable.index(timing) + 1])
                 else:
                     print(f'No ring (muted ring at {timing})')
                     self.last_called_timing = timing
 
+                self.screen.message('Next ring at', 1)
+                self.screen.message(self.next_called_timing)
     def instant_ring(self):
         ring_callbacks.start_ring()
         time.sleep(self.ring_duration)
--- daemon/ring_callbacks.py
+++ daemon/ring_callbacks.py
@@ -7,12 +7,12 @@ port = 25
 
 def start_ring():
     print(colored('🔔 [DAEMON] RING!', 'blue'))
-    os.system(f'sudo echo {port} > /sys/class/gpio/export')
-    os.system(f'sudo echo out > /sys/class/gpio/gpio{port}/direction')
-    os.system(f'sudo echo 1 > /sys/class/gpio/gpio{port}/value')
+    os.system(f'echo {port} > /sys/class/gpio/export')
+    os.system(f'echo out > /sys/class/gpio/gpio{port}/direction')
+    os.system(f'echo 1 > /sys/class/gpio/gpio{port}/value')
 
 def stop_ring():
     print(colored('🔔  [DAEMON STOP RING', 'blue'))
-    os.system(f'sudo echo {port} > /sys/class/gpio/export')
-    os.system(f'sudo echo out > /sys/class/gpio/gpio{port}/direction')
-    os.system(f'sudo echo 0 > /sys/class/gpio/gpio{port}/value')
+    os.system(f'echo {port} > /sys/class/gpio/export')
+    os.system(f'echo out > /sys/class/gpio/gpio{port}/direction')
+    os.system(f'echo 0 > /sys/class/gpio/gpio{port}/value')
